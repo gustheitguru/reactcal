3dacbb4d532e8a0febee35b2372f20cbaf82176f
Gus <gusr@logicliving.com>
2020-09-01 23:13:23 -0500

    Start of react cal

---
+++ src/Application.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+
+function Application(){
+
+     return(
+          <div className="container">
+               <div className="container">Screen</div>
+
+               <button type="button" className="btn btn-outline-success">9</button>
+               <button type="button" className="btn btn-outline-success">8</button>
+               <button type="button" className="btn btn-outline-success">7</button>
+               <button type="button" className="btn btn-outline-success">%</button>
+<br/>
+               <button type="button" className="btn btn-outline-success">6</button>
+               <button type="button" className="btn btn-outline-success">5</button>
+               <button type="button" className="btn btn-outline-success">4</button>
+               <button type="button" className="btn btn-outline-success">x</button>
+               <br/>
+               <button type="button" className="btn btn-outline-success">3</button>
+               <button type="button" className="btn btn-outline-success">2</button>
+               <button type="button" className="btn btn-outline-success">1</button>
+               <button type="button" className="btn btn-outline-success">-</button>
+               <br/>
+               <button type="button" className="btn btn-outline-success">0</button>
+               <button type="button" className="btn btn-outline-success">.</button>
+               <button type="button" className="btn btn-outline-danger">=</button>
+               <button type="button" className="btn btn-outline-success">+</button>
+               <br/>
+               
+               
+               
+               
+          </div>
+     );   
+};
+
+export default Application;
