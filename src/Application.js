55746427368f2ab9d42ed73b791e3f5ed10ad15c
Gus <gusr@logicliving.com>
2020-09-05 14:01:54 -0500

    just need to make the math work

--- src/Application.js
+++ src/Application.js
@@ -1,25 +1,53 @@
 import React, { Component } from 'react';
 import Button from './component/Button';
+import './css/style.css';
 
 class Application extends Component {
      constructor(props) {
           super(props);
 
           this.state = {
-               current: '0',
+               current: '',
                previous: [],
+               nextIsReset: false,
           }
      }
 
 
      reset = () => {
-          this.setState({result: '0'});
+          this.setState({
+               current: '',
+               pervious: [],
+               nextIsReset: false,
+          });
      }
      
      addToCurrent = (symbol) => {
-          this.setState({current: this.state.current + symbol});
+          console.log(symbol)
+          if(["%", "-", "+", "*"].indexOf(symbol) > -1){
+               let {previous} = this.state;
+               console.log(previous)
+               previous.push(this.state.current + symbol);
+               //will use this as Key:Value pair or you can riet {pervious:pervious}
+               this.setState({previous, nextIsReset: true});
+               this.setState({current: ''})
+          } else {
+               if(this.state.current === '0' && symbol != '.'){
+                    this.setState({current: symbol, nextIsReset: false});
+               } else {
+                    this.setState({current: this.state.current + symbol});
+               }
+               
+          }
      }
-
+     
+     calculate = (symbol) => {
+          let {current, pervious, nextIsReset} = this.state;
+          if(pervious.length > 0){
+               current = eval(String(this.state.previous + current));
+               this.setState({current, pervious: [], nextIsReset:true});
+          } 
+     }
 
      render () {
           const buttons = [
@@ -28,8 +56,9 @@ class Application extends Component {
                {symbol: '7', cols:1, action: this.addToCurrent},
                {symbol: '8', cols:1, action: this.addToCurrent},
                {symbol: '9', cols:1, action: this.addToCurrent},
-               {symbol: 'X', cols:1, action: this.addToCurrent},
+               {symbol: '*', cols:1, action: this.addToCurrent},
                {symbol: '4', cols:1, action: this.addToCurrent},
+               {symbol: '5', cols:1, action: this.addToCurrent},
                {symbol: '6', cols:1, action: this.addToCurrent},
                {symbol: '-', cols:1, action: this.addToCurrent},
                {symbol: '1', cols:1, action: this.addToCurrent},
@@ -38,15 +67,20 @@ class Application extends Component {
                {symbol: '+', cols:1, action: this.addToCurrent},
                {symbol: '0', cols:2, action: this.addToCurrent},
                {symbol: '.', cols:1, action: this.addToCurrent},
-               {symbol: '=', cols:1, action: this.addToCurrent},
+               {symbol: '=', cols:1, action: this.calculate},
                ];
 
           return (
                <div className="application container">
+                    {
+                         this.state.previous.length > 0 ? 
+                              <div className="float-last">{this.state.previous}</div>
+                         : null
+                    }
                     <input className="result" type="text" value={this.state.current} />
                    
                    {buttons.map((btn, i)=>{
-                    return <Button symbol={btn.symbol} cols={btn.cols} action={(symbol)=> btn.action} />
+                    return <Button key={i} symbol={btn.symbol} cols={btn.cols} action={(symbol)=> btn.action(symbol)} />
                    })}
                </div>
           );
--- src/component/Button.js
+++ src/component/Button.js
@@ -4,8 +4,8 @@ class Button extends Component {
 	
 	render() {
 		return (
-				<div>
-					<button type="button" class="calc-button btn btn-outline-success">{this.props.symbol}</button>
+				<div className={`column-${this.props.cols}`}>
+					<button type="button" onClick={(e)=> this.props.action(this.props.symbol)} className="calc-button btn btn-outline-success">{this.props.symbol}</button>
 				</div>
 			);
 	}	
